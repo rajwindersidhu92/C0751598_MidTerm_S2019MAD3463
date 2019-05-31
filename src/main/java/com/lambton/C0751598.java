a25ef5f7c183de3eb9d73b93f1411a035eb5c487
MacStudent <macstudent@130-IMAC-34.local>
2019-05-31 19:31:45 -0400

    quest 2

--- src/main/java/com/lambton/C0751598.java
+++ src/main/java/com/lambton/C0751598.java
@@ -5,10 +5,23 @@ public class C0751598 {
         String original = "Lambton";
         String binaryString1 ="111111";
         String binaryString2 ="111136";
+        String name1 = "James tiBeriUs kiRK";
+        String name2 = "AaroN LANGille";
+
+
 
         LambtonStringTools s1 = new LambtonStringTools();
         s1.reverse(original);
 
-        System.out.println("Question 2 if the string is binary")
+        System.out.println("Question 2 if the string is binary");
+        s1.binarytodecimal(binaryString1);
+
+        System.out.println("Question 2 if the string is not binary");
+        s1.binarytodecimal(binaryString2);
+
+        System.out.println("question 3 if name has firstname middle name and lastname");
+        s1.binarytodecimal(name1);
+
+        
     }
 }
--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -14,8 +14,44 @@ public class LambtonStringTools {
     }
 
     public void binarytodecimal(String binaryString){
-        int calculatedDecimalValue;
-        System.out.println("Question 2: Binary String is : "+binaryString);
+        int calculatedDecimalValue = 0;
+        int multiplefactor = 1;
+        boolean flag = false;
+        System.out.println("Input String is : "+binaryString);
+
+        for(int i =0; i < binaryString.length(); i++){
+            if(binaryString.charAt(i) != '0'){
+                if (binaryString.charAt(i) != '1') {
+                    System.out.println("Input String is not binary");
+                    System.out.println("Output is: Null");
+                    flag = true;
+                }
+            }
+            if(flag == true){
+                break;
+            }
+        }
+
+        if(flag == false){
+            System.out.println("its binary");
+            for (int j = binaryString.length()-1 ; j >=0 ; j-- ){
+                calculatedDecimalValue += Character.getNumericValue(binaryString.charAt(j)) * multiplefactor;
+                multiplefactor *=2;
+            }
+            System.out.println("Output is : "+ calculatedDecimalValue);
+        }
+
+
+        public void initials(String name){
+            String initials= "";
+            System.out.println("Question 3: Name is : "+ name);
+
+//            for(int i=original.length()-1 ; i >=0 ; i--){
+//                reversedString = reversedString + original.charAt(i);
+//            }
+//
+//            System.out.println("Answer 1: Reversed String is : "+reversedString);
+        }
 
 
     }
